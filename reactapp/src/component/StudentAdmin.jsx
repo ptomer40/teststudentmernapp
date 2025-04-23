fd714c53d0dbdc0c5d21e65bde0e72e612855b2d
ptomer40 <ptomer40@yahoo.com>
2025-04-23 14:24:34 +0530

    simple Mern app

---
+++ reactapp/src/component/StudentAdmin.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+function StudentAdmin() {
+    const [studentData,setStudentData]=useState([]);
+    async function showData(e){
+        e.preventDefault();
+        const sid=e.target.sid.value;
+    //alert("hiiiii"+sid);
+if(sid=='*'){
+    const response=await fetch("http://localhost:3005/admin/show");
+            const res=await response.json();
+            console.log(res.msg);
+            setStudentData(res.msg);
+}
+else{
+    const response=await fetch(`http://localhost:3005/admin/showByEmailId/${sid}`);
+    const res=await response.json();
+    console.log(res.msg);
+    setStudentData(Array.isArray(res.msg)?res.msg:[res.msg]);
+   }
+}
+
+async function deleteStudent(email){
+//alert(email);
+const response=await fetch(`http://localhost:3005/admin/deleteByEmailId/${email}`,{
+    method:'DELETE'
+});
+    const res=await response.json();
+    alert(res.msg);
+
+}
+function updateStudent(){
+
+}
+  return (
+   <div>
+    <div style={{backgroundColor:'brown',color:'white',fontSize:'25px', margin:'20px'}}>StudentAdmin</div>
+    <form onSubmit={showData}>
+    <div>
+        <input type='text' name='sid' placeholder='Enter * or Student Id' required />
+    </div>
+    <div>
+        <button>Serach Student</button>
+    </div>
+    </form>
+    <div>
+        {
+        studentData && studentData.length>0?
+        (<table border={2}>
+         <thead>
+            <t><td>Name</td><td>Email</td></t>
+         </thead>
+         <tbody>
+            {studentData.map((student,index)=>(
+                <tr key={index}>
+                    <td>{student.name}</td>
+                    <td>{student.email}</td>
+                    <td><button onClick={()=>deleteStudent(student.email)}>Delete</button></td>
+                    <td><button onClick={()=>updateStudent(student.email)}>Update</button></td>
+                    </tr>
+            ))}
+         </tbody>
+        </table>):
+        (<h2>No student availabe</h2>)
+        }
+
+    </div>
+    </div>
+  )
+}
+
+export default StudentAdmin
